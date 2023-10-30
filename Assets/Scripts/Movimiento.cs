58fed263c46757d17002218bd8f212ddb9c82153
feerraan <fazpiazuadrover@gmail.com>
2023-10-30 08:50:58 +0100

    Bastantes coses

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+    [SerializeField] private float speed = 12.5f;
+    [SerializeField] private float verticalSpeed = 10f;
+    [SerializeField] private Vector3 offset = new Vector3(x: 22, y: 0, z: 0);
+
+
+
+    private float verticalinput;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalinput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        transform.Translate(Vector3.up * Time.deltaTime * verticalSpeed * verticalinput);
+
+        camera.transform.position = transform.position + offset;
+    }
+}
